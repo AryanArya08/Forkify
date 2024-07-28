8f31c56d5d7597a1144a8cb2446659f6d4df7197
Aryan <aryanchoubey25@gmail.com>
2024-07-28 12:00:55 +0530

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '62ccd4db-3ffd-45d7-8d53-7699c2653d8b';
+export const MODAL_CLOSE_SEC = 2.5;
